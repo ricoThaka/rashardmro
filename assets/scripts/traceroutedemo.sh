ccaabbaa3cf6a33670a2e9d0468fc62cb43fb0ca
ricoThaka <134652418+ricoThaka@users.noreply.github.com>
2024-12-26 11:50:14 -0800

    https://giovanni.gsfc.nasa.gov/giovanni/doc/UsersManualworkingdocument.docx.html @whitehouse

--- assets/scripts/traceroutedemo.sh
+++ assets/scripts/traceroutedemo.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-e
 
-This is a completed traceroute
-
+echo "This is a completed traceroute"
+traceroute localhost
+echo " Any traceroute you run after this should complete without *** if there are not problems"
+sleep .5 # Waits 0.5 second.
+echo " One Mo GiN " 
+sleep .5 # Waits 0.5 second.
+traceroute 127.0.0.1
+sleep .5 # Waits 0.5 second.
+ping -c 2 localhost
+echo "Nigh der ya go!"
